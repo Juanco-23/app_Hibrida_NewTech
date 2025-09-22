a29eb0efe7eb8f762abf279e7c5c9d69c4c1a2c9
Juanco-23 <j.diego.db@gmail.com>
2025-09-21 23:41:02 -0500

    agregado de Ionic y Angular

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'app_Hibrida_NewTech',
+  webDir: 'www'
+};
+
+export default config;
